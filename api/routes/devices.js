df22f9b3c512ee686fb9221b6792220411e827b4
Kikilma <kikilma.tech@gmail.com>
2021-09-21 00:41:18 -0600

    select-DEVICE

--- api/routes/devices.js
+++ api/routes/devices.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const { checkAuth } = require('../middlewares/authentication.js')
 
-
 /*
  ___  ______________ _____ _      _____ 
 |  \/  |  _  |  _  \  ___| |    /  ___|
@@ -14,8 +13,6 @@ const { checkAuth } = require('../middlewares/authentication.js')
 */
 import Device from '../models/device.js';
 
-
-
 /* 
   ___  ______ _____ 
  / _ \ | ___ \_   _|
@@ -25,11 +22,10 @@ import Device from '../models/device.js';
 \_| |_/\_|    \___/ 
 */
 
-//===============GET-DEVICE==================
+//===============GET-DEVICE========================
 router.get("/device", checkAuth, async (req, res) => {
 
   try {
-
     const userId = req.userData._id;
     const devices = await Device.find({ userId: userId });
 
@@ -37,7 +33,6 @@ router.get("/device", checkAuth, async (req, res) => {
       status: "success",
       data: devices
     };
-
     res.json(toSend);
 
   } catch (error) {
@@ -52,8 +47,7 @@ router.get("/device", checkAuth, async (req, res) => {
     return res.status(500).json(toSend);
   }
 });
-
-//===============NEW-DEVICE==================
+//===============NEW-DEVICE========================
 router.post("/device", checkAuth, async (req, res) => {
   try {
     const userId = req.userData._id;
@@ -63,6 +57,8 @@ router.post("/device", checkAuth, async (req, res) => {
     newDevice.createdTime = Date.now();
 
     const device = await Device.create(newDevice);
+
+    selectDevice(userId, newDevice.dId);
 
     const toSend = {
       status: "success"
@@ -81,10 +77,55 @@ router.post("/device", checkAuth, async (req, res) => {
     return res.status(500).json(toSend);
   }
 });
+//===============DELETE-DEVICE=====================
+router.delete("/device", checkAuth, async(req, res) => {
 
-router.delete("/device", (req, res) => {});
+  try {
+    const userId = req.userData._id;
+    const dId = req.query.dId;
 
-router.put("/device", (req, res) => {});
+    const result = await Device.deleteOne({userId:
+       userId, dId: dId});
+  
+    const toSend = {
+      status: "success",
+      data: result
+    };
+  
+    return res.json(toSend);
+    
+  } catch (error) {
+
+    console.log("ERROR DELETING DEVICE");
+    console.log(error);
+
+    const toSend = {
+      status: "error",
+      error: error
+    };
+
+    return res.status(500).json(toSend);
+  }
+});
+//================UPDATE DEVICE====================
+router.put("/device", checkAuth, (req, res) => {
+  const dId = req.body.dId;
+  const userId = req.userData._id;
+
+  if (selectDevice(userId, dId)) {
+    const toSend = {
+      status: "success"
+    };
+
+    return res.json(toSend);
+  } else {
+    const toSend = {
+      status: "error"
+    };
+
+    return res.json(toSend);
+  }
+});
 
 /* 
 ______ _   _ _   _ _____ _____ _____ _____ _   _  _____ 
@@ -94,5 +135,26 @@ ______ _   _ _   _ _____ _____ _____ _____ _   _  _____
 | |   | |_| | |\  | \__/\ | |  _| |_\ \_/ / |\  |/\__/ /
 \_|    \___/\_| \_/\____/ \_/  \___/ \___/\_| \_/\____/  
 */
+
+async function selectDevice(userId, dId) {
+  try {
+    const result = await Device.updateMany(
+      { userId: userId },
+      { selected: false }
+    );
+
+    const result2 = await Device.updateOne(
+      { dId: dId, userId: userId },
+      { selected: true }
+    );
+
+    return true;
+
+  } catch (error) {
+    console.log("ERROR IN 'selectDevice' FUNCTION ");
+    console.log(error);
+    return false;
+  }
+}
 
 module.exports = router;
