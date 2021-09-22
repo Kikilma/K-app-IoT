37e7e519732e5341c3772c4f61f36158f6ff3dc4
Kikilma <kikilma.tech@gmail.com>
2021-09-21 22:27:58 -0600

    store -- tienda

---
+++ store/index.js
@@ -0,0 +1,11 @@
+export const state = () => ({
+    auth: null,
+    devices: []
+  });
+  
+  
+  export const mutations = {
+      setAuth(state, auth){
+          state.auth = auth;
+      }
+  }
