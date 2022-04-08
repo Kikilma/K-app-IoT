040046b308fc633f25a5d1bf20b4252139a8f366
Kikilma <kikilma.tech@gmail.com>
2022-04-07 23:51:23 -0600

    commit 20

--- api/routes/webhooks.js
+++ api/routes/webhooks.js
@@ -74,14 +74,15 @@ router.post("/getdevicecredentials", async (req, res) => {
     }, 10000);
   } catch (error) {
     console.log(error);
-    res.sendStatus(500);
+    return res.status(500).json();
   }
 }); 
  
 //SAVER WEBHOOK
 router.post("/saver-webhook", async (req, res) => {
   try {
-    if (req.headers.token != process.env.EMQX_API_TOKEN) {      req.sendStatus(404);
+    if (req.headers.token != process.env.WEBHOOK_TOKEN) {
+      res.status(404).json();
       return;
     }
 
@@ -104,22 +105,22 @@ router.post("/saver-webhook", async (req, res) => {
       console.log("Data created");
     }
 
-    res.sendStatus(200);
-  } catch (error) {
+    return res.status(200).json();
+    } catch (error) {
     console.log(error);
-    res.sendStatus(200);
-  }
+    return res.status(500).json();
+    }
 });
 
 //ALARMS WEBHOOK
 router.post("/alarm-webhook", async (req, res) => {
   try {
-    if (req.headers.token != process.env.EMQX_API_TOKEN) {
-      res.sendStatus(404);
+    if (req.headers.token != process.env.WEBHOOK_TOKEN) {
+      res.status(404).jason();
       return;
     }
 
-    res.sendStatus(200);
+    res.status(200).json();
 
     const incomingAlarm = req.body;
 
@@ -289,8 +290,10 @@ function startMqttClient() {
     encoding: "utf8"
   };
 
-  client = mqtt.connect("mqtt://" + process.env.MQTT_NOTIFICATION_HOST, options);
-
+  client = mqtt.connect(
+    "mqtt://" + process.env.MQTT_NOTIFICATION_HOST,
+    options
+  );
   
   client.on("connect", function() {
     console.log("MQTT CONNECTION -> SUCCESS;".green);
